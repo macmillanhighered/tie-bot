0a38ac92967010f1c19c127dadcb3160546a21e6
Jeff Printy <jeff.printy.contractor@macmillan.com>
2018-06-14 21:33:13 -0500

    fix port

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export const log = (() => {
 const app = express();
 app.start = async () => {
   log.info('Starting Server...');
-  const port = 8000;
+  const port = 5000;
   app.set('port', port);
 
   app.use(bodyParser.json());
